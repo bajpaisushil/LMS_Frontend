df83f8b4d03fb8f68a48dd984ca7928c6113cd4c
sushilvxxiv <sushil.bajpai@vxxiv.com>
2023-09-06 17:45:18 +0530

    redux toolkit setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Footer from './components/footer'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className=''>
-      <Footer />
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   )
 }
 
